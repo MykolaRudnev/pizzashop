1bbfaad7bd46c4347464d1ad4b21e967704a2bf9
Mykola Rudnev <RudnevMykola@gmail.com>
2020-04-27 22:29:12 +0200

    added google fonts OpenSans and Righteous

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+body{
+font-family: 'Open Sans', sans-serif;
+}
+
+h1, h2, h3 {
+  font-family: 'Righteous', cursive;
+}
+`
 
 function App() {
   return (
+    < >
+    <h1>Pizza</h1>
+    <GlobalStyle />
     <div>Hello pizza shop</div>
+    /</>
   );
 }
 
