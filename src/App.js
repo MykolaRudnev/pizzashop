fc0622b85f4f95b4dc9f1ab8b0b1297f06763ea9
Mykola Rudnev <RudnevMykola@gmail.com>
2020-04-30 00:22:35 +0200

    Aded Menu Food data and FoodGrid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {createGlobalStyle} from 'styled-components';
-import {Navbar} from './Navbar/Navbar'
-import {Banner} from './Banner/Banner'
+import {Navbar} from './Navbar/Navbar';
+import {Banner} from './Banner/Banner';
+import {Menu} from "./Menu/Menu"
+
 const GlobalStyle = createGlobalStyle`
 body{
   margin: 0;
@@ -19,6 +21,7 @@ function App() {
     <GlobalStyle />
     <Navbar/>
     <Banner/>
+    <Menu/>
     <div>Hello pizza shop</div>
     </>
   );
---
+++ src/Data/FoodData.js
@@ -0,0 +1,40 @@
+export const foods = [
+    {
+        name: 'Barbecue',
+        img: '/img/barbecue.jpg'
+    },
+
+    {
+        name: 'Burger',
+        img: '/img/burger.jpg'
+    },
+
+    {
+        name: 'Pasta',
+        img: '/img/pasta.jpg'
+    },
+
+    {
+        name: 'Pizza',
+        img: '/img/pizza.jpg'
+    },
+
+    {
+        name: 'Rice-crispies',
+        img: '/img/rice-crispies.jpg'
+    },
+
+    {
+        name: 'Sandwich',
+        img: '/img/sandwich.jpg'
+    },
+    {
+        name: 'Spaghetti Sauce',
+        img: '/img/spaghetti-sauce.jpg'
+    },
+
+    {
+        name: 'White Bowl',
+        img: '/img/white-bowl.jpg'
+    },
+]
